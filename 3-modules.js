af244b05202853dfd8b887c877465ec6dd7a066e
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-01-13 13:43:06 -0500

    built multiple module and required them in another js file

---
+++ 3-modules.js
@@ -0,0 +1,14 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require('./4-firstmodule');
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+
+console.log(data);
+console.log(names);
+console.log(sayHi);
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
---
+++ 4-firstmodule.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'john'
+const peter = 'peter'
+
+// This module will only share the names and not the secret variable
+module.exports = {john,peter}
