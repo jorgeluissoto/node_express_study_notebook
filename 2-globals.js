401278efe9a7b304fb2ec59d3f18ad0a247e0d4b
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-01-13 08:23:41 -0500

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// Globals - NO Window !!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use module (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being excuted
+
+console.log(__dirname);
+
+setInterval(() =>{
+  console.log('hello world')
+},1000)
