5b61809f7e7e18047b030c294a98bb876cb58ab0
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-01-14 10:57:58 -0500

    learn how to us built in os module

--- 3-modules.js
+++ 3-modules.js
@@ -4,11 +4,13 @@
 const names = require('./4-firstmodule');
 const sayHi = require("./5-utils");
 const data = require("./6-alternative-flavor");
+require('./7-mind-grenade')
 
-console.log(data);
-console.log(names);
-console.log(sayHi);
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+// console.log(data);
+// console.log(names);
+// console.log(sayHi);
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
